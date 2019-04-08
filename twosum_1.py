04632d316f7d1f1abc8e52e5979f8600ab2d96e2
Vishnualuri <valuri@bu.edu>
2019-04-08 14:36:35 -0400

    initial upload

---
+++ twosum_1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index1 in range(0,len(nums)):
+            for index2 in range(1,len(nums)):
+                if nums[index1]+nums[index2] == target:
+                    if index1 != index2:
+                        return index1,index2
+                        break
+                    else:
+                        pass
+        
+            
